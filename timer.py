bba83af98d3b0a00ce15eb2accaedcb3ba26deb3
Jake <42842918+jwatts4@users.noreply.github.com>
2022-03-08 22:12:48 -0500

    Create new class to handle getting new time from CLI
    Trying to ensure that the Timer class doesn't get overcrowded.

--- timer.py
+++ timer.py
@@ -1,4 +1,6 @@
 import time
+from abc import ABC
+import beepy
 
 
 """
@@ -15,7 +17,8 @@ class Timer:
 
     def display_time(self):
         minutes, seconds = divmod(self.seconds, 60)
-        print("{:02d}:{:02d}".format(minutes, seconds), end='\r')
+        hours, minutes = divmod(minutes, 60)
+        print("{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds), end="\r")
 
     def countdown(self):
         if self.seconds <= 0:
@@ -25,4 +28,27 @@ class Timer:
                 self.display_time()
                 time.sleep(1.0)
                 self.seconds -= 1
+            beepy.beep(sound="coin")
             print("Done!")
+
+
+class TimerSetter(ABC):
+    @staticmethod
+    def set_time(timer):
+        pass
+
+
+class CLITimerSetter(TimerSetter):
+    @staticmethod
+    def set_time(timer):
+        print("Set the timer:")
+        # Get three separate values (hours, minutes, seconds)
+        hours = int(input("Hours:   "))
+        minutes = int(input("Minutes: "))
+        seconds = int(input("Seconds: "))
+
+        # Condense down into a single value (seconds)
+        minutes += hours * 60
+        seconds += minutes * 60
+
+        timer.set_time(seconds)
