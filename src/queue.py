0e68e4b1b0a847db28c3cbc98699b4e3170960fd
Jake Watts <42842918+jwatts4@users.noreply.github.com>
2022-03-19 17:10:22 -0400

    Create a simple queue implementation for use within the Timer class.

---
+++ src/queue.py
@@ -0,0 +1,27 @@
+from copy import deepcopy
+
+
+class Queue:
+    """ Queue implemented with a python list """
+
+    def __init__(self):
+        self._queue = []
+
+    def __len__(self):
+        return len(self._queue)
+
+    def enqueue(self, data):
+        self._queue.append(data)
+
+    def dequeue(self):
+        assert len(self._queue) > 0
+        datum = self._queue.pop(0)
+        return datum
+
+    def is_empty(self):
+        return len(self._queue) == 0
+
+    def peek(self):
+        assert len(self._queue) > 0
+        datum = deepcopy(self._queue[0])
+        return datum
